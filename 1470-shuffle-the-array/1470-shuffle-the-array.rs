7f5f80e9cf19c975f98adf8b881a1b7af2391bb6
osm1892 <osm1892@sungkyul.ac.kr>
2022-02-16 21:14:08 +0900

    Time: 5 ms (31.11%), Space: 2.1 MB (62.22%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.rs
+++ 1470-shuffle-the-array/1470-shuffle-the-array.rs
@@ -1,6 +1,6 @@
 impl Solution {
     pub fn shuffle(nums: Vec<i32>, n: i32) -> Vec<i32> {
-        let mut result: Vec<i32> = Vec::with_capacity(n as usize);
+        let mut result: Vec<i32> = Vec::with_capacity(2 * n as usize);
         for i in 0..n as usize {
             result.push(nums[i]);
             result.push(nums[n as usize + i]);
