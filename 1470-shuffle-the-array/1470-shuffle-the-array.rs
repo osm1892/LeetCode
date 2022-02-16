021bce7e827de51e7a0d9a6c8198bdc231357739
osm1892 <osm1892@sungkyul.ac.kr>
2022-02-16 21:13:08 +0900

    Time: 0 ms (100.00%), Space: 2.1 MB (31.11%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.rs
@@ -0,0 +1,10 @@
+impl Solution {
+    pub fn shuffle(nums: Vec<i32>, n: i32) -> Vec<i32> {
+        let mut result: Vec<i32> = Vec::with_capacity(n as usize);
+        for i in 0..n as usize {
+            result.push(nums[i]);
+            result.push(nums[n as usize + i]);
+        }
+        result
+    }
+}
