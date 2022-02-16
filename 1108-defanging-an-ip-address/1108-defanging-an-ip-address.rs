230ea4c7d67a874e752c813a9b02f90eb5216ec3
osm1892 <osm1892@sungkyul.ac.kr>
2022-02-16 15:43:28 +0900

    Time: 1 ms (25.00%), Space: 2.1 MB (25.00%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.rs
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.rs
@@ -1,12 +1,14 @@
 impl Solution {
     pub fn defang_i_paddr(address: String) -> String {
         let mut split = address.split(".");
-        format!(
-            "{}[.]{}[.]{}[.]{}",
-            split.next().unwrap(),
-            split.next().unwrap(),
-            split.next().unwrap(),
-            split.next().unwrap()
-        )
+        let mut result: Vec<char> = Vec::new();
+
+        for i in 0..3 {
+            result.extend(split.next().unwrap().chars());
+            result.extend("[.]".chars());
+        }
+        result.extend(split.next().unwrap().chars());
+
+        result.iter().collect()
     }
 }
