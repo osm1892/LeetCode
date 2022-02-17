a78c5d21121b209b9191bac11c65862f6a48227d
osm1892 <osm1892@sungkyul.ac.kr>
2022-02-17 11:41:49 +0900

    Time: 1 ms (30.00%), Space: 2.1 MB (67.50%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.rs
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.rs
@@ -1,10 +1,13 @@
 impl Solution {
     pub fn num_identical_pairs(nums: Vec<i32>) -> i32 {
         let mut counter = vec![0; 101];
-        nums.iter().for_each(|&x| counter[x as usize] += 1);
-        counter
-            .into_iter()
-            .reduce(|acc, x| acc + x * (x - 1) / 2)
-            .unwrap()
+        for i in nums {
+            counter[i as usize] += 1;
+        }
+        let mut result = 0;
+        for i in counter {
+            result += i * (i - 1) / 2;
+        }
+        result
     }
 }
