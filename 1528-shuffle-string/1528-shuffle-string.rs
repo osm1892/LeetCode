ad08d72add37ebc47075edb4c8711484f89a76d9
osm1892 <osm1892@sungkyul.ac.kr>
2022-03-01 15:16:20 +0900

    Time: 0 ms (100.00%), Space: 2.2 MB (6.67%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.rs
+++ 1528-shuffle-string/1528-shuffle-string.rs
@@ -1,9 +1,11 @@
 impl Solution {
     pub fn restore_string(s: String, indices: Vec<i32>) -> String {
         let mut result = vec![' '; s.len()];
+        let mut i = 0;
 
-        for i in s.chars().zip(indices) {
-            result[i.1 as usize] = i.0;
+        for c in s.chars() {
+            result[indices[i] as usize] = c;
+            i += 1;
         }
 
         result.into_iter().collect()
