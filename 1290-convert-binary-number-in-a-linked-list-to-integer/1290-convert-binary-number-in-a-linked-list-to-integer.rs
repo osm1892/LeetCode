2228586da9b2f762b98c05e8df45e3080d3ca2fb
osm1892 <osm1892@sungkyul.ac.kr>
2022-03-15 17:37:13 +0900

    Time: 1 ms (38.89%), Space: 2.2 MB (16.67%) - LeetHub

--- 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.rs
+++ 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.rs
@@ -1,16 +1,18 @@
 impl Solution {
     pub fn get_decimal_value(head: Option<Box<ListNode>>) -> i32 {
-        match head {
-            Some(x) => match x.next {
-                Some(mut next) => {
-                    next.val += 2 * x.val;
-                    let result = Solution::get_decimal_value(Option::Some(next.clone()));
-                    next.val -= 2 * x.val;
-                    result
-                }
-                _ => x.val,
-            },
-            _ => 0,
+        let mut result = 0;
+        let mut cursor = &head;
+
+        loop {
+            match cursor {
+                Some(x) => {
+                    result = result * 2 + x.val;
+                    cursor = &x.next;
+                },
+                None => break
+            }
         }
+
+        result
     }
 }
