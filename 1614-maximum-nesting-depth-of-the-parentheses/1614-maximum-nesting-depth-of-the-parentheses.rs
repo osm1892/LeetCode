3217f3a4d91fa41b243e234040360679cbc87928
osm1892 <osm1892@sungkyul.ac.kr>
2022-03-15 16:35:09 +0900

    Time: 2 ms (72.73%), Space: 2 MB (81.82%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.rs
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.rs
@@ -14,7 +14,7 @@ impl Solution {
                 ')' => {
                     depth -= 1;
                 }
-                '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => match s.get(i + 1) {
+                '0'..='9' => match s.get(i + 1) {
                     Some(&x) => {
                         if x.is_numeric() {
                             max_depth = max_depth.max(depth + 1);
