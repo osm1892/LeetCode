cd5ab7dff0e2d365d9545be79c491dd9c860e2c1
osm1892 <osm1892@sungkyul.ac.kr>
2022-04-04 18:15:43 +0900

    Time: 9 ms (13.33%), Space: 2.2 MB (20.00%) - LeetHub

---
+++ 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.rs
@@ -0,0 +1,9 @@
+impl Solution {
+    pub fn max_product_difference(nums: Vec<i32>) -> i32 {
+        let mut nums = nums;
+        nums.sort();
+        let size = nums.len();
+
+        nums[size - 1] * nums[size - 2] - nums[0] * nums[1]
+    }
+}
