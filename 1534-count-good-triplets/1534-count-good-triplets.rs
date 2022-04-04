07155b5484f29da5ab4f3b79162b5b83d7597a18
osm1892 <osm1892@sungkyul.ac.kr>
2022-04-04 18:40:52 +0900

    Time: 6 ms (100.00%), Space: 2.3 MB (50.00%) - LeetHub

---
+++ 1534-count-good-triplets/1534-count-good-triplets.rs
@@ -0,0 +1,22 @@
+impl Solution {
+    pub fn count_good_triplets(arr: Vec<i32>, a: i32, b: i32, c: i32) -> i32 {
+        let mut result = 0;
+
+        for i in 0..arr.len() {
+            for j in (i + 1)..arr.len() {
+                if (arr[i] - arr[j]).abs() > a {
+                    continue;
+                }
+                for k in (j + 1)..arr.len() {
+                    if (arr[j] - arr[k]).abs() > b {
+                        continue;
+                    }
+                    if (arr[i] - arr[k]).abs() <= c {
+                        result += 1;
+                    }
+                }
+            }
+        }
+        result
+    }
+}
